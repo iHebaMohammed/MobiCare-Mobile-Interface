92749c28fb17a0432f044f2c7c952b3901b6928f
MohammedMoataz <imohammedmoataz@gmail.com>
2023-06-07 03:09:16 +0300

    add first aid tips feature

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,25 @@
-import 'package:bloc/bloc.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:mobi_care/layouts/doctor_layout/doctor_layout.dart';
-import 'package:mobi_care/layouts/patient_layout/cubit/cubit.dart';
-import 'package:mobi_care/layouts/patient_layout/cubit/states.dart';
-import 'package:mobi_care/modules/doctor_modules/patient_profile_doctor_view/cubit/cubit.dart';
-import 'package:mobi_care/modules/patinet_modules/register/register_screen.dart';
-import 'package:mobi_care/shared/bloc_observer.dart';
-import 'modules/doctor_modules/doctor_time_reminder/cubit/cubit.dart';
-import 'modules/doctor_modules/patient_profile_doctor_view/patient_profile_doctor_view_screen.dart';
-import 'modules/patinet_modules/doctor_profile_patient_view/doctor_profile_patient_view_screen.dart';
+
+import 'layouts/patient_layout/cubit/cubit.dart';
+import 'layouts/patient_layout/cubit/states.dart';
 import 'modules/patinet_modules/patient_edit_profile/cubit/cubit.dart';
 import 'modules/patinet_modules/patient_medication_reminder/cubit/cubit.dart';
 import 'modules/patinet_modules/patient_prescriptions/cubit/cubit.dart';
 import 'modules/patinet_modules/patient_profile/cubit/cubit.dart';
-import 'modules/patinet_modules/patient_profile/patient_profile_screen.dart';
+
+import 'modules/doctor_modules/patient_profile_doctor_view/cubit/cubit.dart';
+import 'modules/doctor_modules/doctor_time_reminder/cubit/cubit.dart';
+
 import 'modules/shared_modules/chat_details/cubit/cubit.dart';
 import 'modules/shared_modules/login/login_screen.dart';
 import 'modules/shared_modules/payment/cubit/cubit.dart';
+
+import 'shared/network/local/cache_helper.dart';
 import 'shared/network/remote/dio_helper.dart';
 import 'shared/styles/themes.dart';
-import 'shared/network/local/cache_helper.dart';
+import 'shared/bloc_observer.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -34,8 +32,7 @@ void main() async {
 }
 
 class MyApp extends StatelessWidget {
-
-   const MyApp({super.key});
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -45,20 +42,20 @@ class MyApp extends StatelessWidget {
           create: (context) => PatientLayoutCubit()..getChats(),
         ),
         BlocProvider(
-            create: (context) => ChatMessagesCubit(),
+          create: (context) => ChatMessagesCubit(),
         ),
         BlocProvider(
           create: (context) => PaymentCubit(),
         ),
         BlocProvider(
-            create: (context) => PatientProfileCubit()..getNewAccessToken(),
+          create: (context) => PatientProfileCubit()..getNewAccessToken(),
         ),
         BlocProvider(
           create: (context) => PatientEditProfileCubit(),
         ),
         BlocProvider(
-          create: (context) => PatientMedicationReminderCubit()..createDatabase()
-        ),
+            create: (context) =>
+                PatientMedicationReminderCubit()..createDatabase()),
         BlocProvider(
           create: (context) => DoctorTimeReminderCubit()..createDatabase(),
         ),
